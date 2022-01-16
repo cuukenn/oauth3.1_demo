f3de3b9c18291745b14c38c57eb8e67d6c82b571
changgg <891842749@qq.com>
2022-01-16 14:16:12 +0800

    chore: init the project

---
+++ project-admin-ums/ums-server/src/main/java/com/project/ums/server/web/UserManageController.java
@@ -0,0 +1,12 @@
+package com.project.ums.server.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author changgg
+ */
+@RestController
+@RequestMapping("/user-manage")
+public class UserManageController {
+}
